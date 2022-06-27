2fbd368371cece74ca2b260f16b9c4fe2308ead8
jvallexm <jvallexm@gmail.com>
2022-06-27 10:22:53 -0400

    Dynamic check for manual address prefixes

--- main.tf
+++ main.tf
@@ -37,7 +37,6 @@ module "vpcs" {
   region                       = var.region
   tags                         = var.tags
   resource_group_id            = data.ibm_resource_group.resource_group[each.value.resource_group].id
-  use_manual_address_prefixes  = each.value.use_manual_address_prefixes == null ? false : each.value.use_manual_address_prefixes
   classic_access               = each.value.classic_access
   default_network_acl_name     = each.value.default_network_acl_name
   default_security_group_name  = each.value.default_security_group_name
@@ -48,6 +47,17 @@ module "vpcs" {
   use_public_gateways          = each.value.use_public_gateways
   subnets                      = each.value.subnets
   vpn_gateway                  = each.value.vpn_gateway
+  use_manual_address_prefixes  = each.value.use_manual_address_prefixes == true ? true : length([
+    # If use manual address prefixes is not set check each zone
+    for zone in ["zone-1", "zone-2", "zone-3"] :
+    true if( 
+      each.value.address_prefixes[zone] == null        # if prefix is null
+      ? false                                          # false
+      : length(each.value.address_prefixes[zone]) == 0 # if the length of prefixes is 0
+      ? false                                          # false
+      : true                                           # if zone has more than one prefix set to true
+    )                                                  # return true
+  ]) != 0                                              # true if prefixes are being used, false if not
 }
 
 ##############################################################################
